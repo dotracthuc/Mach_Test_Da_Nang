6b5c173e630cebe2c1951d8e6df80ad9ae3d4792
dotracthuc <dotracthuc10@gmail.com>
2019-06-03 18:37:27 +0900

    Lan dau tien

---
+++ README.md
@@ -0,0 +1,6 @@
+# CanhBaoCauThangHshopLTK
+
+TakeOneServer.ino, TakeOneClient.ino use for communicating 2 client with Server
+https://www.youtube.com/watch?v=a7wEuzupfQc
+
+Thuc da xem qua
---
+++ examples/Prototype_Frame_Program/Program_Manager.h
@@ -0,0 +1,14 @@
+#ifndef _PROGRAME_MANAGER_H_H_
+#define _PROGRAME_MANAGER_H_H_
+#include "Program_Adaptor.h"
+
+enum{
+  HELLOWORLD_PROGRAM_ID = 0, // THE FIRST MUST BE 0!
+  LAST_PROGRAM_ID
+};
+
+extern Program_Adaptor_Class * p_ListProgram[LAST_PROGRAM_ID];
+
+extern void programManager_setup();
+extern void programManager_loop();
+#endif
